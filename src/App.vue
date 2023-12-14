f7ff86da4a7ee56de8476c672d45a083ec001e58
Atif-Bashir-1998 <adobe8832289@gmail.com>
2023-12-14 21:39:16 +0500

    HomeView created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
   <router-view></router-view>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,9 @@
 <template>
-  <p>Welcome</p>
+  <div class="flex flex-col space-y-4 h-screen w-full justify-center items-center">
+    <router-link to="/one">One</router-link>
+    <router-link to="/two">Two</router-link>
+    <router-link to="/three">Three</router-link>
+    <router-link to="/four">Four</router-link>
+    <router-link to="/five">Five</router-link>
+  </div>
 </template>
