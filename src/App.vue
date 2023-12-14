ca469c3804b40c4f2fe2d6fece9203fba8a6614e
Atif-Bashir-1998 <adobe8832289@gmail.com>
2023-12-14 21:16:42 +0500

    Tailwind CSS configured to the project

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script></script>
 
 <template>
-  <p>Hello world</p>
+  <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
