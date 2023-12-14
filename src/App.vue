c0eb9dd0dccaedb98a75938c94c3344f5efe3bee
Atif-Bashir-1998 <adobe8832289@gmail.com>
2023-12-14 21:36:03 +0500

    code cleanup and used router-view in the app

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script></script>
-
 <template>
   <h1 class="text-3xl font-bold underline bg-red-500">Hello world!</h1>
+  <router-view></router-view>
 </template>
+
+<script script>
+</script>
 
 <style scoped></style>
