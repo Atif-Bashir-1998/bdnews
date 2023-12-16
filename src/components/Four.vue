53c8cfd7ef1cf62b3b81d2db52e21e29c4f2b86d
Atif-Bashir-1998 <adobe8832289@gmail.com>
2023-12-16 11:30:30 +0500

    component 4 created

--- src/components/Four.vue
+++ src/components/Four.vue
@@ -1,3 +1,99 @@
 <template>
-  <p>Four</p>
+  <div class="px-4 md:px-64 py-16 mt-16 bg-gray-100">
+    <Swiper :modules="modules" :navigation="true" :pagination="pagination" class="h-96">
+      <SwiperSlide v-for="news in spotlightNews">
+        <div class="h-full grid md:grid-cols-2 border-2">
+          <img class="" :src="news.image" />
+
+          <div class="flex flex-col gap-4 w-full h-full justify-center md:px-16">
+            <p class="font-bold text-2xl">{{ news.title }}</p>
+
+            <p class="text-gray-700">{{ news.description }}</p>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+// Import Swiper Vue.js components
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const spotlightNews = ref([
+  {
+    image: "https://cdn.wallpapersafari.com/67/82/U76fMj.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+  {
+    image: "https://i.pinimg.com/originals/09/6a/35/096a35453660aa9b83ba4ab6d9182d61.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+  {
+    image: "https://wallpapers.com/images/hd/blue-aesthetic-moon-df8850p673zj275y.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+  {
+    image: "https://wallpapers.com/images/hd/1920x1080-aesthetic-11zwee5mu1ol66fp.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+  {
+    image: "https://i.pinimg.com/originals/5c/15/6f/5c156fca5df8bdfcb66e7f7c319a5384.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+  {
+    image: "https://wallpapercave.com/wp/wp2818490.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+  {
+    image: "https://i.redd.it/o3frxgu8h6191.jpg",
+    title: "France has found British boy missing since 2017",
+    description: "A teenager from Britain who disappeared six years ago has been found in southern France, fleeing a spiritualist mountain community"
+  },
+])
+
+const modules = ref([Navigation, Pagination])
+
+const pagination = ref({
+  clickable: true,
+  renderBullet: function (index, className) {
+    return '<span class="' + className + '"></span>';
+  },
+})
+</script>
+
+<style>
+swiper-container::part(bullet-active) {
+  background-color: rgb(180, 18, 18);
+}
+
+.swiper-button-next, .swiper-button-prev {
+  background-color: rgba(0,0,0);
+  color: white;
+  height: 14px;
+  width: 14px;
+  padding: 20px;
+  border-radius: 100%;
+}
+
+.swiper-button-next:after, .swiper-button-prev:after{
+  font-size: 12px;
+}
+
+.swiper-pagination-bullet-active{
+  background: red !important;
+}
+</style>
